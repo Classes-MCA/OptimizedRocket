9adccc9b9e21d469274dbdb37f81128f1d5aa1e5
Mark C. Anderson <anderson.mark.az@gmail.com>
2021-02-27 07:41:19 -0700

    initial commit

---
+++ src/getDrag.m
@@ -0,0 +1,15 @@
+% Purpose: To calculate the drag on the vehicle
+%
+% Inputs:
+%   - velocity: the velocity of the rocket in (x,y) coordinates, measured
+%               meters per second
+%   - altitude: the altitude of the rocket in meters
+%
+% Outputs:
+%   - drag: the total drag force on the rocket in Newtons
+
+function [drag] = getDrag(velocity,altitude)
+
+    
+
+end
